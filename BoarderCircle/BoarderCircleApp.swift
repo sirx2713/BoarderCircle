a631f4641c613ddc4228da46ffeb1fb8adabc9ac
iSAD <bradx2713@gmail.com>
2024-07-04 22:24:04 +0200

    Initial Commit

---
+++ BoarderCircle/BoarderCircleApp.swift
@@ -0,0 +1,17 @@
+//
+//  BoarderCircleApp.swift
+//  BoarderCircle
+//
+//  Created by Tafadzwa Alexander on 2024/07/04.
+//
+
+import SwiftUI
+
+@main
+struct BoarderCircleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
